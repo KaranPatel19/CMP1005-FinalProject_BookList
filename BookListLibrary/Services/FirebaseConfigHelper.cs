8f6917313650575acd5bf43e13ff21848380b336
karanpatel <A00264306@mycambrian.ca>
2023-04-12 18:31:21 -0400

    adding FirebaseConfig for the connection of Database.

---
+++ BookListLibrary/Services/FirebaseConfigHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+
+namespace BookListLibrary.Services
+{
+	public class FirebaseConfigHelper
+	{
+        public static IFirebaseConfig Config = new FirebaseConfig
+        {
+            AuthSecret = "PVXETWHDsuE7HsjKBi9nZlbbt2fw6BEzbZWdSG4c",
+            BasePath = "https://booklist-90d5d-default-rtdb.firebaseio.com/"
+        };
+    }
+}
