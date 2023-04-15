e00ec5136566abfa45aef12890b8925c4bb302c4
karanpatel <A00264306@mycambrian.ca>
2023-04-14 20:26:54 -0400

    XUnit Test - implemented Xunittest
    Made XUnitTest Project and Implemented test for it

---
+++ BookListXunitTest/BookListServiceXunitTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+using Moq;
+using FluentAssertions;
+using BookListLibrary.Data;
+using BookListLibrary.Services;
+using FireSharp.Interfaces;
+
+public class BookListServiceXunitTests
+{
+    [Fact]
+    public async Task AddBookAsync_ShouldAddBookSuccessfully()
+    {
+        // Arrange
+        var mockClient = new Mock<IFirebaseClient>();
+        var service = new BookListService(mockClient.Object);
+        var newBook = new BookList
+        {
+            Title = "Test Book",
+            Author = "Test Author",
+            Genre = "Test Genre",
+            Description = "Test Description"
+        };
+
+        // Act
+        await service.AddBookAsync(newBook);
+
+        // Assert
+        mockClient.Verify(client => client.PushAsync("BookList/", newBook), Times.Once);
+    }
+
+    
+}
---
+++ BookListXunitTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace BookListXunitTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
