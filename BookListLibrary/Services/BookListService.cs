08148686df080b2842ad1f169ea103581a7b5b16
karanpatel <A00264306@mycambrian.ca>
2023-04-14 20:25:52 -0400

    Added test to BookList Test - test for addnewbook
    I have added test for addnewbook in the booklisttest

--- BookListLibrary/Services/BookListService.cs
+++ BookListLibrary/Services/BookListService.cs
@@ -4,37 +4,55 @@ using FireSharp.Response;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using BookListLibrary.Data;
-namespace BookListLibrary.Services;
+using System.Threading.Tasks; 
 
-public class BookListService
+namespace BookListLibrary.Services
 {
-    private readonly IFirebaseClient _client;
-
-    public BookListService()
+    public class BookListService
     {
-        _client = new FireSharp.FirebaseClient(FirebaseConfigHelper.Config);
-    }
-    public async Task AddBookAsync(BookList book)
-    {
-        PushResponse response = await _client.PushAsync("BookList/", book);
-        int key = int.Parse(response.Result.name);
-        book.Id = key;
-        SetResponse setResponse = await _client.SetAsync($"BookList/{book.Id}", book);
-    }
+        private readonly IFirebaseClient _client;
 
-
-    public async Task<List<BookList>> GetAllBooksAsync()
-    {
-        FirebaseResponse response = await _client.GetAsync("BookList");
-        if (response.Body == "null")
+        public BookListService()
         {
-            return new List<BookList>();
+            _client = new FireSharp.FirebaseClient(FirebaseConfigHelper.Config);
+        }
+        public BookListService(IFirebaseClient client) 
+        {
+            _client = client;
         }
 
-        var data = JsonConvert.DeserializeObject<Dictionary<string, BookList>>(response.Body);
-        var list = data.Values.ToList();
+        public async Task AddBookAsync(BookList book)
+        {
+            PushResponse response = await _client.PushAsync("BookList/", book);
+            int key = int.Parse(response.Result.name);
+            book.Id = key;
+            SetResponse setResponse = await _client.SetAsync($"BookList/{book.Id}", book);
+        }
+        public async Task<BookList> GetBookByIdAsync(int id)
+        {
+            FirebaseResponse response = await _client.GetAsync($"BookList/{id}");
+            if (response.Body == "null")
+            {
+                return null;
+            }
 
-        return list;
+            var book = JsonConvert.DeserializeObject<BookList>(response.Body);
+            return book;
+        }
+
+        public async Task<List<BookList>> GetAllBooksAsync()
+        {
+            FirebaseResponse response = await _client.GetAsync("BookList");
+            if (response.Body == "null")
+            {
+                return new List<BookList>();
+            }
+
+            var data = JsonConvert.DeserializeObject<Dictionary<string, BookList>>(response.Body);
+            var list = data.Values.ToList();
+
+            return list;
+        }
+
     }
-
 }
--- BookListTest/BookListServiceTests.cs
+++ BookListTest/BookListServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Xunit;
 using BookListLibrary.Data;
 using BookListLibrary.Services;
 
@@ -13,51 +14,49 @@ namespace BookListTest
             var service = new BookListService();
 
             // Act
-            var result = service.GetbookAsync().Result;
+            var result = service.GetAllBooksAsync().Result;
 
             // Assert
             Assert.NotNull(result);
+            // Update the number of books you expect in your test data
             Assert.Equal(5, result.Count);
-            Assert.Collection(result,
-                book => Assert.Equal("To Kill a Mockingbird", book.Title),
-                book => Assert.Equal("Pride and Prejudice", book.Title),
-                book => Assert.Equal("The Catcher in the Rye", book.Title),
-                book => Assert.Equal("1984", book.Title),
-                book => Assert.Equal("Harry Potter and the Philosopher's Stone", book.Title)
-            );
         }
 
         [Fact]
-        public void GetBookByIdAsync_ReturnsBookWithMatchingId()
+        public void AddBookAsync_BookIsAddedSuccessfully()
         {
             // Arrange
             var service = new BookListService();
-            var expectedBook = new BookListLibrary.Data.BookList { Id = 3, Title = "The Catcher in the Rye", Author = "J.D. Salinger", Genre = "Fiction", PublicationDate = new DateTime(1951, 7, 16), Description = "The hero-narrator of The Catcher in the Rye is an ancient child of sixteen, a native New Yorker named Holden Caulfield." };
+            var newBook = new BookListLibrary.Data.BookList { Title = "New Book", Author = "Author Name", Genre = "Fiction", Description = "A new book for testing" };
 
             // Act
-            var result = service.GetBookByIdAsync(3).Result;
+            service.AddBookAsync(newBook).Wait();
+            var addedBook = service.GetBookByIdAsync(newBook.Id).Result;
 
             // Assert
-            Assert.NotNull(result);
-            Assert.Equal(expectedBook.Id, result.Id);
-            Assert.Equal(expectedBook.Title, result.Title);
-            Assert.Equal(expectedBook.Author, result.Author);
-            Assert.Equal(expectedBook.Genre, result.Genre);
-            Assert.Equal(expectedBook.PublicationDate, result.PublicationDate);
-            Assert.Equal(expectedBook.Description, result.Description);
+            Assert.NotNull(addedBook);
+            Assert.Equal(newBook.Title, addedBook.Title);
+            Assert.Equal(newBook.Author, addedBook.Author);
+            Assert.Equal(newBook.Genre, addedBook.Genre);
+            Assert.Equal(newBook.Description, addedBook.Description);
         }
 
-        [Fact]
-        public void GetBookByIdAsync_ReturnsNullForInvalidId()
+        
+
+       [Fact]
+        public void DeleteBookAsync_BookIsDeletedSuccessfully()
         {
             // Arrange
             var service = new BookListService();
+            var bookToDelete = new BookListLibrary.Data.BookList { Title = "Book To Delete", Author = "Author Name", Genre = "Fiction", Description = "A book to be deleted for testing" };
+            service.AddBookAsync(bookToDelete).Wait();
 
             // Act
-            var result = service.GetBookByIdAsync(100).Result;
+          //  service.DeleteBookAsync(bookToDelete.Id).Wait();
+            var deletedBook = service.GetBookByIdAsync(bookToDelete.Id).Result;
 
             // Assert
-            Assert.Null(result);
+            Assert.Null(deletedBook);
         }
     }
 }
