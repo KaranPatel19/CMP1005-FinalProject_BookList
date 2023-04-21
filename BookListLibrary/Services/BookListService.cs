4277cf7f7c04a5c19c345cf113c611d373fef1bf
karanpatel <A00264306@mycambrian.ca>
2023-04-21 00:57:35 -0400

    Updated BookListService
    Updated BookListService (Updating books which are added)

--- BookListLibrary/Services/BookListService.cs
+++ BookListLibrary/Services/BookListService.cs
@@ -1,11 +1,15 @@
 ﻿using FireSharp.Config;
 using FireSharp.Interfaces;
 using FireSharp.Response;
+using Firebase.Database;
 using Newtonsoft.Json;
+using FireSharp.EventStreaming;
+
 using Newtonsoft.Json.Linq;
 using BookListLibrary.Exceptions;
 using BookListLibrary.Data;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 
 namespace BookListLibrary.Services;
 
@@ -20,16 +24,24 @@ public class BookListService
    
    public async Task AddBookAsync(BookList book)
     {
-        PushResponse response = await _client.PushAsync("BookList/", book);
-        int key = int.Parse(response.Result.name);
-        book.Id = key;
+       // PushResponse response = await _client.PushAsync("BookList/", book);
+        //int key = int.Parse(response.Result.name);
+        //book.Id = key;
+
         SetResponse setResponse = await _client.SetAsync($"BookList/{book.Id}", book);
     }
-
-    public object AddBookAsync(object id)
+    public async Task SaveBookAsync(int Id, string Title, string Author, string Genre, string Description)
     {
-        throw new NotImplementedException();
+        var book = new BookList { Id = Id, Title = Title, Author = Author, Genre = Genre, Description = Description };
+        await _client.SetAsync($"BookList/{Id}", book);
     }
+
+    public async Task DeleteBookAsync(int Id)
+    {
+       await _client.DeleteAsync($"BookList/{Id}");
+
+    }
+
 
     public async Task<List<BookList>> GetAllBooksAsync()
     {
